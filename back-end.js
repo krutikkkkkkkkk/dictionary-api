149b0df579afc2552ee8b7f6e99d37c741034ff7
Krutik <58563133+reboot13-git@users.noreply.github.com>
2021-01-12 20:09:28 +0530

    Update back-end.js

--- back-end.js
+++ back-end.js
@@ -1,18 +1,16 @@
 function Search() {  
    var word = document.getElementById("input").value;
-    let api = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;  
+   let api = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;  
       
     fetch(api)  
          .then(function(response){  
             let data = response.json();  
             return data;
-
         }) 
 .then(function(data){ 
 
 //Input
  document.getElementById('word').innerHTML = word;
-
  ///If no Definition
           message = data.message;
           if (message){
@@ -24,7 +22,8 @@ function Search() {
             example1 = data[0].meanings[0].definitions[0].example;
 document.getElementById('definition1').innerHTML ="1:" + definition1;
 document.getElementById('example1').innerHTML = "1:" +  example1;
-  ///audio
+
+///audio
 voiceUrl = data[0].phonetics[0].audio;
 var audio = document.getElementById('audio');
 var src = document.createAttribute("src");
@@ -36,9 +35,6 @@ audio.setAttributeNode(src);
      if(definition2 != null) {
   document.getElementById('definition2').innerHTML =  "2:" + definition2;
   document.getElementById('example2').innerHTML = "2:" +  example2;
-    }
-    
-
-
+    }  
         })  
 }
